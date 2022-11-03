4889dda08864a7b5ed095188824be56bfe4c6654
Gakuto1112 <gakuto131112@gmail.com>
2022-11-03 14:31:36 +0900

    情報パネルの描画

--- static/TabletClock.js
+++ static/TabletClock.js
@@ -16,12 +16,12 @@ function onToggleFullscreenButtonClick() {
 //時計の表示を更新する。
 function refreshClock() {
 	const dateTime = new Date();
-	document.getElementById("clock_area_date_month").innerText = dateTime.getMonth() + 1;
-	document.getElementById("clock_area_date_date").innerText = dateTime.getDate();
+	document.getElementById("date_month").innerText = dateTime.getMonth() + 1;
+	document.getElementById("date_date").innerText = dateTime.getDate();
 	const dayName = ["日", "月", "火", "水", "木", "金", "土"];
-	document.getElementById("clock_area_date_day").innerText = dayName[dateTime.getDay()];
-	document.getElementById("clock_area_time_hour").innerText = dateTime.getHours();
-	document.getElementById("clock_area_time_minute").innerText = `0${dateTime.getMinutes()}`.slice(-2);
+	document.getElementById("date_day").innerText = dayName[dateTime.getDay()];
+	document.getElementById("time_hour").innerText = dateTime.getHours();
+	document.getElementById("time_minute").innerText = `0${dateTime.getMinutes()}`.slice(-2);
 	console.info("時計が更新されました。");
 }
 
