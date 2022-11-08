f6c73b410959905b816e8e32e94004b9539a20d9
Gakuto1112 <gakuto131112@gmail.com>
2022-11-08 19:09:28 +0900

    センサーの交換 ﾅﾝｶｾﾝｻｰﾉｱﾀｲｶﾞｵｶｼｲﾖｳﾅ...

--- Sensors.ts
+++ Sensors.ts
@@ -1,11 +1,10 @@
 import fs from "fs";
 import { parse } from "jsonc-parser";
-import * as i2c from "i2c-bus";
+import i2c from "i2c-bus";
 const mcpadc: any = require("mcp-spi-adc");
 
 export interface SensorsConfigObject {
 	busNumber: number;
-	adt7410Address: number;
 	temperatureHumiditySensorInterval: number;
 	brightnessSensorInterval: number;
 	darkModeThreshold: number;
@@ -18,27 +17,66 @@ export class Sensors {
 	 */
 	private readonly config: SensorsConfigObject = parse(fs.readFileSync("config/sensors.jsonc", "utf-8"));
 
+	public constructor() {
+		i2c.openPromisified(this.config.busNumber).then((bus: i2c.PromisifiedBus) => {
+			bus.readByte(0x38, 0x71).then((initData: number) => {
+				if(initData != 0x18) {
+					Promise.all([0x1b, 0x1c, 0x1e].map((byte: number) => {
+						return new Promise<void>((resolve, reject) => {
+							bus.i2cWrite(0x38, 4, Buffer.from([0x70, byte, 0x00, 0x00])).then(() => {
+								setTimeout(() => {
+									bus.i2cWrite(0x38, 1, Buffer.from([0x71])).then(() => {
+										bus.i2cRead(0x38, 3, Buffer.alloc(3)).then((data: i2c.BytesRead) => {
+											setTimeout(() => {
+												bus.i2cWrite(0x38, 4, Buffer.from([0x70, 0xb8, data.buffer[1], data.buffer[2]])).then(() => {
+													resolve();
+												});
+											}, 10);
+										});
+									});
+								}, 5);
+							});
+						});
+					}));
+				}
+			});
+		});
+	}
+
+	/**
+	 * センサーの測定トリガーを書き込む。
+	 * @return {Promise<void>}
+	 */
+	public triggerMeasurement(): Promise<void> {
+		return new Promise((resolve, reject) => {
+			i2c.openPromisified(this.config.busNumber).then((bus: i2c.PromisifiedBus) => {
+				bus.i2cWrite(0x38, 3, Buffer.from([0xac, 0x33, 0x00])).then(() => setTimeout(() => resolve(), 80)).catch((error: any) => reject(error));
+			}).catch((error: any) => reject(error));
+		});
+	}
+
 	/**
 	 * センサーから温度を取得する。
 	 * @return {Promise<number>} センサーから取得した温度
 	 */
 	public getTemperature(): Promise<number> {
 		return new Promise((resolve, reject) => {
-			i2c.openPromisified(this.config.busNumber).then((bus: i2c.PromisifiedBus) => {
-				bus.readWord(this.config.adt7410Address, 0x00).then((wordData: number) => {
-					let temperature: number = ((wordData & 0xff00) >> 8 | (wordData & 0xff) << 8) >> 3;
-					temperature = Number(((temperature & 0x1000) == 0 ? temperature * 0.0625 : ((~ temperature & 0x1fff) + 1) * -0.0625).toFixed(1));
-					console.group("[Sensors]: 温度情報を取得しました。");
-					console.debug(`温度：${temperature}℃`);
-					console.groupEnd();
-					resolve(temperature);
+			this.triggerMeasurement().then(() => {
+				i2c.openPromisified(this.config.busNumber).then((bus: i2c.PromisifiedBus) => {
+					bus.i2cRead(0x38, 7, Buffer.alloc(7)).then((data: i2c.BytesRead) => {
+						const temperature: number = Number(((((data.buffer[3] & 0xf) << 16) + (data.buffer[4] << 8) + (data.buffer[5])) / Math.pow(2, 20) * 200 - 50).toFixed(1));
+						console.group("[Sensors]: 温度情報を取得しました。");
+						console.debug(`温度：${temperature}℃`);
+						console.groupEnd();
+						resolve(temperature);
+					}).catch((error: any) => {
+						console.error("[Sensors]: 温度情報の取得に失敗しました。");
+						reject(error);
+					});
 				}).catch((error: any) => {
 					console.error("[Sensors]: 温度情報の取得に失敗しました。");
 					reject(error);
 				});
-			}).catch((error: any) => {
-				console.error("[Sensors]: 温度情報の取得に失敗しました。");
-				reject(error);
 			});
 		});
 	}
@@ -48,12 +86,24 @@ export class Sensors {
 	 * @return {Promise<number>} センサーから取得した湿度
 	 */
 	public getHumidity(): Promise<number> {
-		return new Promise((resolve) => {
-			//現在、湿度センサーを取り寄せ中で手元にないので、取り敢えず固定値を返す。
-			console.group("[Sensors]: 湿度情報を取得しました。");
-			console.debug("湿度：0%");
-			console.groupEnd();
-			resolve(0);
+		return new Promise((resolve, reject) => {
+			this.triggerMeasurement().then(() => {
+				i2c.openPromisified(this.config.busNumber).then((bus: i2c.PromisifiedBus) => {
+					bus.i2cRead(0x38, 7, Buffer.alloc(7)).then((data: i2c.BytesRead) => {
+						const humidity: number = Number((((data.buffer[1] << 12) + (data.buffer[2] << 4) + ((data.buffer[3] & 0xf0) >> 4)) / Math.pow(2, 20) * 100).toFixed(1));
+						console.group("[Sensors]: 湿度情報を取得しました。");
+						console.debug(`湿度：${humidity}℃`);
+						console.groupEnd();
+						resolve(humidity);
+					}).catch((error: any) => {
+						console.error("[Sensors]: 湿度情報の取得に失敗しました。");
+						reject(error);
+					});
+				}).catch((error: any) => {
+					console.error("[Sensors]: 湿度情報の取得に失敗しました。");
+					reject(error);
+				});
+			});
 		});
 	}
 
