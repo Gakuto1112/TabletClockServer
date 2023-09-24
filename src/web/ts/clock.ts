685d90a1c5866cbcc72201a4c9153e5bf7d204f7
Gakuto1112 <gakuto131112@gmail.com>
2023-09-25 02:10:24 +0900

    html要素型をより詳しく

--- src/web/ts/clock.ts
+++ src/web/ts/clock.ts
@@ -13,24 +13,24 @@ export class Clock {
             const now: Date = new Date();
             setTimeout(updateClock, 1000 - now.getMilliseconds());
 
-            const monthElement: HTMLElement = (document.getElementById("clock_month") as HTMLElement);
+            const monthElement: HTMLSpanElement = (document.getElementById("clock_month") as HTMLSpanElement);
             const monthName: string = new Intl.DateTimeFormat("en-US", {month: "short"}).format(now);
             if(monthElement.innerText.slice(0, -1) != monthName) monthElement.innerText = `${monthName}.`;
-            const dayElement: HTMLElement = (document.getElementById("clock_day") as HTMLElement);
+            const dayElement: HTMLSpanElement = (document.getElementById("clock_day") as HTMLSpanElement);
             const day: number = now.getDate();
             if(Number(dayElement.innerText.slice(0, -2)) != day) {
                 const firstDigit: number = day % 10;
                 dayElement.innerText = `${day}${firstDigit == 0 || firstDigit >= 4 || Math.floor(day / 10) == 1 ? "th" : (firstDigit == 1 ? "st" : (firstDigit == 2 ? "nd" : "rd"))}`;
-                (document.getElementById("clock_weekday") as HTMLElement).innerText = `${new Intl.DateTimeFormat("en-US", {weekday: "short"}).format(now)}.`;
+                (document.getElementById("clock_weekday") as HTMLSpanElement).innerText = `${new Intl.DateTimeFormat("en-US", {weekday: "short"}).format(now)}.`;
             }
-            const hourElement: HTMLElement = (document.getElementById("clock_hour") as HTMLElement);
+            const hourElement: HTMLSpanElement = (document.getElementById("clock_hour") as HTMLSpanElement);
             const hour = now.getHours();
             if(Number(hourElement.innerText) != hour) hourElement.innerText = hour.toString();
-            const minuteElement: HTMLElement = (document.getElementById("clock_minute") as HTMLElement);
+            const minuteElement: HTMLSpanElement = (document.getElementById("clock_minute") as HTMLSpanElement);
             const minute: number = now.getMinutes();
             if(Number(minuteElement.innerText) != minute) minuteElement.innerText = `0${minute.toString()}`.slice(-2);
 
-            const dotElement: HTMLElement = (document.getElementById("clock_dot") as HTMLElement);
+            const dotElement: HTMLSpanElement = (document.getElementById("clock_dot") as HTMLSpanElement);
             if(now.getSeconds() % 2 == 0) dotElement.classList.add("clock_blink_animation");
             else dotElement.classList.remove("clock_blink_animation");
         }
