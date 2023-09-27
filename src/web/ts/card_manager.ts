62853fd1e01c442af8129978f6cd9c813447af1e
Gakuto1112 <gakuto131112@gmail.com>
2023-09-27 23:25:06 +0900

    カードの内容もフリップするように変更

--- src/web/ts/card_manager.ts
+++ src/web/ts/card_manager.ts
@@ -1,10 +1,5 @@
 export class CardManager {
     /**
-     * カードの枚数
-     */
-    private readonly cardCount: number = 3;
-
-    /**
      * 現在のカード
      */
     private currentCard: number = 0;
@@ -13,7 +8,8 @@ export class CardManager {
      * 実行関数
      */
     public run(): void {
-        if(this.cardCount > 1) {
+        const cardElement: HTMLDivElement = document.getElementById("card") as HTMLDivElement;
+        if(cardElement.childElementCount > 1) {
             /**
              * カードバーがいっぱいになった時に実行される関数
              * @param currentCardVar 現在のカード
@@ -21,28 +17,32 @@ export class CardManager {
             function onProgressComplete(currentCardVar: number): void {
                 if(!cardElement.classList.contains("card_flip_animation")) {
                     cardElement.classList.add("card_flip_animation");
-                    const currentElement: HTMLDivElement = (cardBarParent.children.item(currentCardVar) as HTMLDivElement);
+                    (cardElement.children.item((currentCardVar + 1) % cardElement.childElementCount) as HTMLDivElement).classList.add("card_back");
+                    const currentElement: HTMLDivElement = cardBarParent.children.item(currentCardVar) as HTMLDivElement;
                     currentElement.classList.remove("card_bar_progression");
                     currentElement.classList.add("card_bar_progression_end");
                 }
             }
 
-            const cardElement: HTMLDivElement = (document.getElementById("card") as HTMLDivElement);
             cardElement.addEventListener("click", () => onProgressComplete(this.currentCard));
-            const cardBarParent: HTMLDivElement = (document.getElementById("card_bar") as HTMLDivElement);
-            for(let i = 0; i < this.cardCount; i++) {
+            const cardBarParent: HTMLDivElement = document.getElementById("card_bar") as HTMLDivElement;
+            for(let i = 0; i < cardElement.childElementCount; i++) {
                 const barElement: HTMLDivElement = document.createElement("div");
                 if(i == 0) window.requestAnimationFrame(() => barElement.classList.add("card_bar_progression"));
                 barElement.addEventListener("transitionend", () => onProgressComplete(this.currentCard));
                 barElement.addEventListener("animationend", () => {
                     cardElement.classList.remove("card_flip_animation");
+                    (cardElement.children.item(this.currentCard) as HTMLDivElement).classList.remove("card_front");
                     (cardBarParent.children.item(this.currentCard) as HTMLDivElement).classList.remove("card_bar_progression_end");
-                    this.currentCard = (this.currentCard + 1) % this.cardCount;
+                    this.currentCard = (this.currentCard + 1) % cardElement.childElementCount;
+                    const currentCardElement: HTMLDivElement = cardElement.children.item(this.currentCard) as HTMLDivElement;
+                    currentCardElement.classList.add("card_front");
+                    currentCardElement.classList.remove("card_back");
                     (cardBarParent.children.item(this.currentCard) as HTMLDivElement).classList.add("card_bar_progression");
                 });
                 cardBarParent.appendChild(barElement);
             }
-            document.documentElement.style.setProperty("--card-bar-width", `${500 / this.cardCount - 20}px`);
+            document.documentElement.style.setProperty("--card-bar-width", `${500 / cardElement.childElementCount - 20}px`);
         }
         else (document.getElementById("card_bar") as HTMLDivElement).style.visibility = "hidden";
     }
